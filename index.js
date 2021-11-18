ba800d780bfef61e34db4b6a7b553f2bbfe0c95c
Vladimir Muratov <vladimirmuratov@mail.ru>
2021-11-18 13:40:08 +0300

    add span and h1

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ console.log('Hello World')
 
 createContainer()
 
-function createContainer(){
+function createContainer() {
     const body = document.querySelector('body')
 
     const container = document.createElement('div')
     container.setAttribute('id', 'container')
-    container.textContent = 'Это контейнер'
+    container.innerHTML = "<p>Это контейнер</p>"
+
+    const title = document.createElement('h1')
+    title.innerText = 'Заголовок'
+
+    const date = document.createElement('span')
+    setInterval(() => {
+        date.innerText = new Date().toLocaleString('ru')
+    }, 1000)
+
+    container.prepend(title)
+    container.append(date)
+
     body.append(container)
 }
