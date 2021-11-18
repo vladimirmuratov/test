8fb2f91442570a6ea3605548a769ec6db3ad7294
Vladimir Muratov <vladimirmuratov@mail.ru>
2021-11-18 13:02:42 +0300

    create div id container

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('Hello World')
+
+createContainer()
+
+function createContainer(){
+    const body = document.querySelector('body')
+
+    const container = document.createElement('div')
+    container.setAttribute('id', 'container')
+    container.textContent = 'Это контейнер'
+    body.append(container)
+}
